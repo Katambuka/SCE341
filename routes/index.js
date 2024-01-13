adee815238ff70da875ddfab1ffbbe5f3a0e14ff
Katambuka <kan22001@byui.edu>
2024-01-14 03:20:13 +0400

    router updates

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello Instructor, this is my first project');
+  res.send('Hello Instructor, this is my first project, I have connected mongodb database but its still playing me, I am working hard to fix everything');
 });
 
 router.use('/users', require('./users'));
