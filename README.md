6071616947ecd961f533b8db81e44dca9c544e0e
Katambuka <kan22001@byui.edu>
2024-01-20 01:51:42 +0400

    ReadMe updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SCE341
+
+#On testing my database on mongodb run this on render
+
+#render url = <https://project1-opu7.onrender.com>
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello Instructor,this is my first project.');
-  console.log('Project still hard to understand but I am progressing')
+  res.send('Hello Instructor,this is my first project.')
 });
 
 router.use('/users', require('./users'));
